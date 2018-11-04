62a5da77112328dc527d5434a9b6e09ce999f9b9
manuelWeb <devries.webdesigner@gmail.com>
2018-11-04 18:15:51 +0100

    keyboard.js add keyboard_container -> simplification of code by jsx

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -3,16 +3,15 @@ import './keyboard.css'
 
 let alphachar  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
-const Keyboard = () => {
-  const keyboard = alphachar.map((letter, idx) => {
-    return (
-      <div key={idx}>
-        <span className="keysymb">{letter}</span>
-      </div>
-    )
-  })
-  return keyboard
-
-}
+const Keyboard = () => (
+  <div className="keyboard">
+    {alphachar.map((letter, idx) => (
+        <div key={idx}>
+          <span className="keysymb">{letter}</span>
+        </div>
+      )
+    )}
+  </div>
+)
 
 export default  Keyboard
