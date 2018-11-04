df16099e4d3ca63f11e14223f0a7f480713fd738
manuelWeb <devries.webdesigner@gmail.com>
2018-11-04 18:29:42 +0100

    add keyboard.css; TODO -> keyboard to rwd @show Goetter book or link in keybd.css

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -4,11 +4,11 @@ import './keyboard.css'
 let alphachar  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
 const Keyboard = () => (
-  <div className="keyboard">
-    {alphachar.map((letter, idx) => (
-        <div key={idx}>
-          <span className="keysymb">{letter}</span>
-        </div>
+  <div className="keyboard_container">
+    { alphachar.map((letter, idx) => (
+      <div key={idx}>
+        <span className="keysymb">{letter}</span>
+      </div>
       )
     )}
   </div>
