7593fd0061ddd66e9bbe5c6572c244b24e7f2a53
macfly <manuel@webdesignord.fr>
2018-11-05 15:59:56 +0100

    add var kbd in Keyboard component to wrap var alphachar

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import './keyboard.css'
 
-let alphachar  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
-const Keyboard = () => (
-  <div className="keyboard_container">
+const Keyboard = () => {
+  const alphachar  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  const kbd = (<div className="keyboard_container">
     { alphachar.map((letter, idx) => (
       <div key={idx}>
         <span className="keysymb">{letter}</span>
       </div>
       )
     )}
-  </div>
-)
+  </div>)
+  return kbd
+}
+
 
 export default  Keyboard
