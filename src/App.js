a4f7259a329b03a12d205ca356b28251b625ecf7
manuelWeb <devries.webdesigner@gmail.com>
2018-11-05 21:12:33 +0100

    init mask fct; TODO add container to display flex-row

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import './App.css';
 import Keyboard from './Keyboard'
 
 
+const allword = ["NOMBRE","GEANTE","CORAUX","ROULEAU","EJECTER","LIVRETS", "DIVISION","LICORNES","FOURNEAU","EMPLETTE","CLEPSYDRE","INDIGENES", "ECLATANTE","MATERIAUX","ANAGRAMME","ULTERIEURE","FACTORISER", "RACCROCHER","HIPPOPOTAME","SAUTERELLES"]
+
+let chosenWord = Math.floor(Math.random() * allword.length)
+chosenWord = allword[chosenWord]
+
+// const Mask = () => {
+//   const letters = chosenWord.split('');
+//   console.log(letters);
+//   letters.map( (letter,idx) => {
+//     return (
+//       <div>
+//         <span className="letter" key={idx}>
+//           {letter}
+//         </span>
+//       </div>
+//     )
+//   })
+// }
+const Mask = () => {
+  const letters = chosenWord.split('');
+    return (
+      letters.map( (letter,idx) => (
+        <span key={idx} className="letter">
+          {letter}
+        </span>)
+      )
+    )
+}
+console.log(chosenWord);
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <h1>Hangman react exo</h1>
+        <Mask />
         <Keyboard />
       </div>
     );
