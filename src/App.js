ea51df4df7d31681072e2ec966a27b8fc30f24f4
macfly <manuel@webdesignord.fr>
2018-11-06 12:02:18 +0100

    protection of variables related to the Mask module

--- src/App.js
+++ src/App.js
@@ -2,38 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Keyboard from './Keyboard'
 
-
-const allword = ["NOMBRE","GEANTE","CORAUX","ROULEAU","EJECTER","LIVRETS", "DIVISION","LICORNES","FOURNEAU","EMPLETTE","CLEPSYDRE","INDIGENES", "ECLATANTE","MATERIAUX","ANAGRAMME","ULTERIEURE","FACTORISER", "RACCROCHER","HIPPOPOTAME","SAUTERELLES"]
-
-let chosenWord = Math.floor(Math.random() * allword.length)
-chosenWord = allword[chosenWord]
-
-// const Mask = () => {
-//   const letters = chosenWord.split('');
-//   console.log(letters);
-//   letters.map( (letter,idx) => {
-//     return (
-//       <div>
-//         <span className="letter" key={idx}>
-//           {letter}
-//         </span>
-//       </div>
-//     )
-//   })
-// }
 const Mask = () => {
+  const allword = ["NOMBRE","GEANTE","CORAUX","ROULEAU","EJECTER","LIVRETS", "DIVISION","LICORNES","FOURNEAU","EMPLETTE","CLEPSYDRE","INDIGENES", "ECLATANTE","MATERIAUX","ANAGRAMME","ULTERIEURE","FACTORISER", "RACCROCHER","HIPPOPOTAME","SAUTERELLES"]
+  let chosenWord = Math.floor(Math.random() * allword.length)
+  chosenWord = allword[chosenWord]
   const letters = chosenWord.split('');
-    return (
-      <div>
-      {letters.map( (letter,idx) => (
-        <span key={idx} className="letter">
-          {letter}
-        </span>)
-      )}
-      </div>
-    )
+  console.log(chosenWord);
+  return (
+    <div>
+    {letters.map( (letter,idx) => (
+      <span key={idx} className="letter">
+        {letter}
+      </span>)
+    )}
+    </div>
+  )
 }
-console.log(chosenWord);
 
 class App extends Component {
 
